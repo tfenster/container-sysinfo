7c702f6de50d9927935b2c63e635644a8fe9d528
Tobias Fenster <tobias.fenster@cosmoconsult.com>
2021-04-11 17:05:14 +0000

    add pathprefix for reverse proxy

--- Startup.cs
+++ Startup.cs
@@ -34,6 +34,12 @@ namespace container_sysinfo
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            var pathPrefix = Environment.GetEnvironmentVariable("PathPrefix");
+            if (!string.IsNullOrEmpty(pathPrefix))
+            {
+                app.UsePathBase($"/{pathPrefix}");
+            }
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
